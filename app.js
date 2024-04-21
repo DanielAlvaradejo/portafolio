208ec9c728e7d207383e16d567228bb69abda0a4
Daniel Alvaradejo Silva <84923265+DanielAlvaradejo@users.noreply.github.com>
2024-04-21 09:24:46 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -14,3 +14,29 @@ function topFunction() {
   document.body.scrollTop = 0; // Para navegadores Safari
   document.documentElement.scrollTop = 0; // Para otros navegadores
 }
+// Función para enviar correos
+
+window.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('contact-form');
+
+  // Inicializar EmailJS con tu clave pública
+  emailjs.init('D9brYC83E-6VeuSCB');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obtener los datos del formulario
+    const formData = new FormData(form);
+
+    // Enviar el formulario con EmailJS
+    emailjs.sendForm('service_zrmspr9', 'template_aurccim', form)
+      .then(function() {
+        console.log('Mensaje enviado con éxito');
+        form.reset();
+        enviarWhatsApp(formData);
+      }, function(error) {
+        console.error('Error al enviar el mensaje:', error);
+      });
+  });
+
+});
